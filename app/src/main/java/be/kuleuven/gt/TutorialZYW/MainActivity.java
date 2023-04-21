3cf9f67aebd24e632707f09fc4a3202bad825b64
98262 <982625148zzj@gmail.com>
2023-04-21 12:02:53 +0200

    hahaha woshinidie

--- app/src/main/java/be/kuleuven/gt/TutorialZYW/MainActivity.java
+++ app/src/main/java/be/kuleuven/gt/TutorialZYW/MainActivity.java
@@ -2,13 +2,37 @@ package be.kuleuven.gt.TutorialZYW;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button btnPlus;
+    private Button btnMinus;
+    private TextView lblQty;
+    private int quantity;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnMinus = (Button) findViewById(R.id.btnMinus);
+        lblQty = (TextView) findViewById(R.id.lblQty);
+        quantity = Integer.parseInt(lblQty.getText().toString());
     }
+    public void onBtnPlus_Clicked(View Caller) {
+        quantity = quantity + 1;
+        lblQty.setText(Integer.toString(quantity)); }
+
+    public void onBtnMinus_Clicked(View Caller) {
+
+        if(Integer.parseInt(lblQty.getText().toString())>0){
+            quantity -= 1;}
+
+        lblQty.setText(Integer.toString(quantity)); }
 }
